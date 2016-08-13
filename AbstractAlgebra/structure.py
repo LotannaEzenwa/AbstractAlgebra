f7594517bdd11be7eac96855687291acc733e02f
LotannaEzenwa <lezenwa@quantopian.com>
2016-08-13 17:54:26 -0400

    Added basic structure no pun intended

---
+++ AbstractAlgebra/structure.py
@@ -0,0 +1,12 @@
+from abc import ABCMeta, absractproperty
+class Structure(metaclass=ABCMeta):
+	"""
+	An Abstract Base Class for an Algebraic Structure
+	Defined as in https://en.wikipedia.org/wiki/Algebraic_structure
+
+	Each Structure has both a set and a mathematical object attached to it
+	"""
+	def __init__(self, set_obj, math_obj):
+		pass
+
+	
