1af220566ccb506cc56c4dfc070f131438596fe2
LotannaEzenwa <lezenwa@quantopian.com>
2016-08-15 00:42:17 -0400

    Added descriptors for axioms

--- AbstractAlgebra/set.py
+++ AbstractAlgebra/set.py
@@ -13,8 +13,11 @@ class Set(object):
 		if (elements is not None) and (rule is not None):
 			raise NotImplementedError("Set cannot be instantiated both elements and rule")
 
-	
+	def __iter__(self)
+		return self
 
+	def next(self):
+		raise NotImplementedError
 
 	
 
@@ -22,18 +25,25 @@ class Extensionality(Axiom):
 	"""
 	Defines the extensionality Axiom for set theory:
 	Two sets are equal (are the same set) if they have the same elements.
+
+	Implemented as Structure implements "Equals"
 	"""
-	def __init__(self):
-		pass
+	def __init__(self, structure=None):
+
+	@classmethod
+	def override_equals(cls):
+		raise NotImplementedError
 
 	@staticmethod
 	def confirm(structure):
-		raise NotImplementedError
+		return 
 
 class Regularity(Axiom):
 	"""
 	Defines the Regularity Axiom for set Theory:
 	Every non-empty set x contains a member y such that x and y are disjoint sets.
+
+
 	"""
 
 	def __init__(self):
@@ -47,6 +57,9 @@ class Specification(Axiom):
 	"""
 	Defines the Axiom of Schema Specification:
 	Elements of a Set can be specified using a rule
+
+
+	Implemented as structure implements "generator"
 	"""
 	def __init__(self):
 		pass
@@ -60,6 +73,8 @@ class Pairing(Axiom):
 	Defines the Axiom of Pairing:
 	If x and y are sets, then there exists a set which contains x and y as elements.
 	(Implemented that two sets can be elements of another set)
+
+
 	"""
 	def __init__(self):
 		pass
@@ -84,6 +99,8 @@ class Replacement(Axiom):
 	"""
 	Defines the Axiom Schema of Replacement:
 	The axiom schema of replacement asserts that the image of a set under any definable function will also fall inside a set.
+
+	Functions on some structure must return a valid structure
 	"""
 	def __init__(self):
 		pass
@@ -97,6 +114,8 @@ class Infinity(Axiom):
 	Defines the Axiom of Infinity:
 	A set an be built be having an empty set unioned with y unioned with a set containing itself.
 	Or, There exists a set having infintely many members
+
+	Structure can implement an infinite generator
 	"""
 	def __init__(self):
 		pass
@@ -109,6 +128,8 @@ class PowerSet(Axiom):
 	"""
 	Defines the Axiom of the Power Set:
 	The Axiom of Power Set states that for any set x, there is a set y that contains every subset of x:
+
+
 	"""
 	def __init__(self):
 		pass
